f33b4de4ed3a769ce7e85bd9b3df4041be8df65e
Stefan <stefan@creative-tim.com>
2022-10-19 16:03:44 +0300

    refactor: erase comp

--- src/components/js/dashboardContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// this is the equivalent to the createStore method of Redux
-// https://redux.js.org/api/createstore
-
-const DashboardContext = React.createContext({} as any);
-
-export default DashboardContext;
