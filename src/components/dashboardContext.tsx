15dbd0222eb5a30560dc0f3a555c23ec4be883a6
Stefan <stefan@creative-tim.com>
2022-10-19 16:04:22 +0300

    feat: add new components

---
+++ src/components/dashboardContext.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// this is the equivalent to the createStore method of Redux
+// https://redux.js.org/api/createstore
+
+const DashboardContext = React.createContext({} as any);
+
+export default DashboardContext;
