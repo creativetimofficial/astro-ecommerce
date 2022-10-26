f29c9a720bcc9774dcd26adf2dba718b9c2a8e56
Stefan <stefan@creative-tim.com>
2022-10-26 14:02:00 +0300

    refactor: update card product comp

--- src/components/products/cardProduct.tsx
+++ src/components/products/cardProduct.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
+import ProductBadge from './productBadge';
 
 interface Props {
   img_src: string;
   img_alt: string;
   title: string;
+  description: string;
   price: string;
   color: string;
   colors: string[];
   position: string;
 }
 
-function badge(color:string) {
-  const badgeClass = "badge filter bg-" + color;
-  const badge = <span className={badgeClass}></span>
-  
-  return badge;
-}
-
 export default function CardProduct({
   img_src,
   img_alt,
   title,
+  description,
   price,
   color,
   colors,
@@ -28,27 +24,37 @@ export default function CardProduct({
 }: Props) {
 
   const classList = "card-body " + "text-" + position;
-  const badges = <div>{colors.map(color => badge(color))}</div>;
 
   return (
     <>
       <div className="card card-product border border-white mb-5 shadow-xs">
-        {/* <div className="height-300 w-100 rounded-top" style={{backgroundImage: `url(${img_src})`, backgroundSize: 'cover'}}></div> */}
         <a href="#">
           <div className="height-300">
             <img className="w-100 h-100 rounded-top" src={img_src} alt={img_alt} />
           </div>
           <div className={classList}>
-            <p className="text-sm mb-1 text-body">{color}</p>
-            <h5 className="mb-0 font-weight-bold">
-              {title}
-            </h5>
-            <p className="mb-0 text-sm text-body mt-1">
-              {price}
-            </p>
-            <div className="mt-4">
-              {badges}
-            </div>
+            {(color) && 
+              <p className="text-sm mb-1 text-body">{color}</p>
+            }
+            {(title) && 
+              <h5 className="font-weight-bold">
+                {title}
+              </h5>
+            }
+
+            {(description) && 
+              <p className="text-body text-sm">{description}</p>
+            }
+           
+            {(price) && 
+              <p className="mb-0 text-sm text-body mt-1 mb-3">
+                {price}
+              </p>
+            }
+           
+            {(colors) &&
+              <ProductBadge colors={colors} />
+            }
           </div>
         </a>
       </div>
