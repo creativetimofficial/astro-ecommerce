6190b7d6f7c6b02458a69b651f5d534f380d0867
Stefan <stefan@creative-tim.com>
2022-10-31 21:25:52 +0200

    refactor: update product overviews

--- src/components/products/productOverviews.tsx
+++ src/components/products/productOverviews.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProductBadge from './productBadge';
 import ProductRating from './productRating';
 import ProductAccordion from './productAccordion';
+import ProductImages from './productImages';
 
 interface Props {
   title: string;
@@ -29,34 +30,41 @@ export default function ProductOverviews({
     <>
     <div className="card card-product card-plain">
       <div className="row">
+        {(images.length != 0) && 
+          <ProductImages images={images}/>
+        }
         <div className="col-12 col-md-6">
-          <img className="w-90 rounded-2" src={images[0]} alt="first image" />
-          <div className="d-flex mt-4">
-            <img className="w-20 me-4 rounded-2" src={images[0]} alt="first image" />
-            <img className="w-20 me-4 rounded-2" src={images[1]} alt="first image" />
-            <img className="w-20 me-4 rounded-2" src={images[2]} alt="first image" />
-            <img className="w-20 rounded-2" src={images[3]} alt="first image" />
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <h2>{title}</h2>
-          <div className="d-flex">
-            <h3 className="font-weight-normal">{price}</h3>
-            <input className="opacity-0" defaultValue={price} />
-          </div>
-          <ProductRating rating={rating} reviews={reviews} />
-          <p className="mt-4">{full_description}</p>
-          <h6 className="mt-4">Color:</h6>
-          {(colors) &&
-            <ProductBadge colors={colors} />
+          {(title.length != 0) && 
+            <h2>{title}</h2>
           }
+          {(price.length != 0) && 
+            <>
+              <div className="d-flex mb-3">
+                <h3 className="font-weight-normal">{price}</h3>
+                <input className="opacity-0" defaultValue={price} />
+              </div>
+            </>
+          }
+          {(rating != 0) && 
+            <ProductRating rating={rating} reviews={reviews} />
+          }
+          <p className="mt-4">{full_description}</p>
+          {(colors.length != 0) && 
+            <>
+              <h6 className="mt-4">Color:</h6>
+              {(colors) &&
+                <ProductBadge colors={colors} />
+              }
+            </>
+          }
+          
           <div className="d-flex align-items-center mt-4">
             <button className="btn btn-primary btn-lg mb-0 me-4">Add to Bag</button>
             <a href="#">
               <i className="far fa-heart text-2xl"></i>
             </a>
           </div>
-            {/* <ProductAccordion data={data} /> */}
+            <ProductAccordion data={data} />
         </div>
       </div>
     </div>
