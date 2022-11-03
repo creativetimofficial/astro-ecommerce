3300ac73cf728e5a6d937e495b21c2faafb85442
Stefan <stefan@creative-tim.com>
2022-11-03 21:28:52 +0200

    refactor: update product cart item 2

--- src/components/cart/productCartItem2.tsx
+++ src/components/cart/productCartItem2.tsx
@@ -22,23 +22,32 @@ export default function CartItem({
   return (
     <>
       <div className="d-flex mt-4">
-        <img className="w-20 rounded-3 m-4" src={thumb_src} alt={thumb_alt} />
+        <img className="w-10 rounded-3 shadow-xs m-4" src={thumb_src} alt={thumb_alt} />
         <div className="w-50 pt-4 ps-4">
           <h6 className="text-lg mb-1">{title}</h6>
             <p className="mb-0">{color}</p>
             <p className="mb-0">{size}</p>
 
           <div className="d-flex align-items-center mt-6">
-            <i className="fas fa-check text-lg text-success"></i>
-            <p className="mb-0 ms-2 text-sm">{stock}</p>
+           {(title.length != 0) ? 
+            <>
+              <i className="fas fa-minus-circle text-lg"></i>
+              <p className="mb-0 ms-2 text-sm">Out of Stock</p>
+            </>
+            :
+            <>
+              <i className="fas fa-check text-lg text-success"></i>
+              <p className="mb-0 ms-2 text-sm">In Stock</p>
+            </>
+           }
           </div>
         </div>
-        <div className="w-10 pt-4">
-          <input type="text" className="form-control mb-3" placeholder="1" aria-label="amount" />
+        <div className="w-5 pt-4">
+          <input type="number" min={0} className="form-control mb-3" placeholder="1" aria-label="amount" />
           <a className="text-primary" href="#">Remove</a>
         </div>
-        <div className="w-20 pt-4 text-end">
-          <h6 className="mb-1">${price}</h6>
+        <div className="w-25 pt-4 text-end">
+          <h6 className="mb-1">${price.toFixed(2)}</h6>
         </div>
       </div>
     </>
