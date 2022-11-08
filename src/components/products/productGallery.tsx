eb2e87959b330785eb952bd80bdacea4fb36123f
Stefan <stefan@creative-tim.com>
2022-11-08 20:10:54 +0200

    refactor: update images - from json object

--- src/components/products/productGallery.tsx
+++ src/components/products/productGallery.tsx
@@ -1,5 +1,8 @@
 interface Props {
-  images: string[];
+  images: ({
+    src: string;
+    alt: string;
+  })[];
 }
 
 export default function ProductGallery({
@@ -10,14 +13,14 @@ export default function ProductGallery({
     <>
       <div className="row">
         <div className="col-4">
-          <img className="w-100 rounded-2" src={images[0]} alt="first image" />
+          <img className="w-100 rounded-2" src={images[0].src} alt={images[0].alt} />
         </div>
         <div className="col-4">
-          <img className="w-100 rounded-2" src={images[1]} alt="first image" />
-          <img className="w-100 mt-4 rounded-2" src={images[2]} alt="first image" />
+          <img className="w-100 rounded-2" src={images[1].src} alt={images[1].alt} />
+          <img className="w-100 mt-4 rounded-2" src={images[2].src} alt={images[2].alt} />
         </div>
         <div className="col-4">
-          <img className="w-100 rounded-2" src={images[3]} alt="first image" />
+          <img className="w-100 rounded-2" src={images[3].src} alt={images[3].alt} />
         </div>
       </div>  
     </>
