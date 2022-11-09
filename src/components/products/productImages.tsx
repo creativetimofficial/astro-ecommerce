8be5a0cb23b521bfa73cf7b781c34e6b2273d55c
Stefan <stefan@creative-tim.com>
2022-11-09 11:48:54 +0200

    refactor: update productImages

--- src/components/products/productImages.tsx
+++ src/components/products/productImages.tsx
@@ -1,5 +1,8 @@
 interface Props {
-  images: string[];
+  images: ({
+    src: string;
+    alt: string;
+  })[];
 }
 
 export default function ProductImages({
@@ -9,12 +12,12 @@ export default function ProductImages({
   return (
     <>
       <div className="col-12 col-md-6">
-        <img className="w-90 rounded-2" src={images[0]} alt="first image" />
+        <img className="w-90 rounded-2" src={images[0].src} alt={images[0].alt} />
         <div className="d-flex mt-4">
-          <img className="w-20 me-4 rounded-2" src={images[0]} alt="first image" />
-          <img className="w-20 me-4 rounded-2" src={images[1]} alt="first image" />
-          <img className="w-20 me-4 rounded-2" src={images[2]} alt="first image" />
-          <img className="w-20 rounded-2" src={images[3]} alt="first image" />
+          <img className="w-20 me-4 rounded-2" src={images[0].src} alt={images[0].alt} />
+          <img className="w-20 me-4 rounded-2" src={images[1].src} alt={images[1].alt} />
+          <img className="w-20 me-4 rounded-2" src={images[2].src} alt={images[2].alt} />
+          <img className="w-20 rounded-2" src={images[3].src} alt={images[3].alt} />
         </div>
       </div> 
     </>
