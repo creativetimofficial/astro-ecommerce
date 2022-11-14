f653f72e9976336e849031528024e9cdbe722b18
Stefan <stefan@creative-tim.com>
2022-11-15 00:17:18 +0200

    refactor: update  order summery

--- src/components/checkout/checkoutOrderSummary.tsx
+++ src/components/checkout/checkoutOrderSummary.tsx
@@ -1,84 +1,71 @@
-import StoreNavbar from './storeNavbar';
+import PaymentDetails from './paymentDetails';
+import ShippingInfo from './shippingInfo';
+import BillingInfo from './billingInfo';
 import OrderSummary from '../cart/orderSummary';
+import CheckoutSingleItem from '../checkout/checkoutSingleItem';
 
 interface Props {
-  title: string;
-  full_description: string;
-  subtotal: number;
-
+  products: ({
+    thumb_src: string;
+    thumb_alt: string;
+    color: string;
+    title: string;
+    price: number;
+    size: string;
+    stock: string;
+    subtotal: number;
+    shipping: number;
+    tax: number;
+  })[];
+  textColor: string
 }
 
 export default function CheckoutSummary({
-  title,
-  full_description,
-  subtotal
+ products,
+ textColor
 }: Props) {
+
+  let subtotalCheckout = 0;
+  products.map(product => 
+    subtotalCheckout += product.price
+  )
 
   return (
     <>
       <section>
         <div className="row">
-          <div className="col-6 p-5">
+          <div className="col-12 col-lg-6 p-5">
             <h5 className="mb-4">Contact information</h5>
             <div className="form-group">
               <label>Email address</label>
               <input type="email" className="form-control" placeholder="name@example.com" />
             </div>
             <h5 className="mt-5 mb-4">Payment details</h5>
-            <div className="form-group">
-              <label>Card Number</label>
-              <input type="text" className="form-control" placeholder="EUR89BTRL1234XXXXX" />
-            </div>
-            <div className="row">
-              <div className="col-8">
-                <div className="form-group">
-                  <label>Expiration date (MM/YY)</label>
-                  <input type="text" className="form-control" placeholder="11/27" />
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="form-group">
-                  <label>CVC</label>
-                  <input type="text" className="form-control" placeholder="112" />
-                </div>
-              </div>
-            </div>
-            <h5 className="mt-5 mb-4">Shipping address</h5>
-            <div className="form-group">
-              <label>Address</label>
-              <input type="text" className="form-control" placeholder="Your Address" />
-            </div>
-            <div className="row">
-              <div className="col-4">
-                <div className="form-group">
-                  <label>City</label>
-                  <input type="text" className="form-control" placeholder="Miami" />
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="form-group">
-                  <label>State / Province</label>
-                  <input type="text" className="form-control" placeholder="California" />
-                </div>
-              </div>
-              <div className="col-4">
-                <div className="form-group">
-                  <label>Postal Code</label>
-                  <input type="text" className="form-control" placeholder="121001" />
-                </div>
-              </div>
-            </div>
-            <h5 className="mt-5 mb-4">Billing information</h5>
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" checked />
-              <label className="custom-control-label">Same as shipping information</label>
-            </div>
-          </div>
-          <div className="col-6 p-5 bg-dark rounded-end">
-            <small className="text-white opacity-6">Amount due</small>
-            <h3 className="text-white">$232.00</h3>
+            <PaymentDetails />
 
-            <OrderSummary subtotal={subtotal}/>
+            <h5 className="mt-5 mb-4">Shipping address</h5>
+            <ShippingInfo />
+
+            <h5 className="mt-5 mb-4">Billing information</h5>
+            <BillingInfo />
+
+            <hr className="dark horizontal"/>
+            <button className="btn btn-primary float-end mt-2 mb-0">Pay now</button>
+          </div>
+          <div className="col-12 col-lg-6 p-5 bg-dark bg-gradient rounded-end">
+            <small className="text-white opacity-6">Amount due</small>
+            <h3 className="text-white mb-4">${subtotalCheckout.toFixed(2)}</h3>
+            {products.map((product, i) => 
+                <CheckoutSingleItem
+                  thumb_src={product.thumb_src}
+                  thumb_alt={product.thumb_alt}
+                  title={product.title}
+                  color={product.color}
+                  size={product.size}
+                  price={product.price}
+                />
+            )}
+            <OrderSummary subtotal={subtotalCheckout} textColor="white" />
           </div>
         </div>
       </section>
