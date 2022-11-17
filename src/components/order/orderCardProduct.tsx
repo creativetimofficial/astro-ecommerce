fcf11f8e80fe809f8de36560383a8e759ad0e8f7
Stefan <stefan@creative-tim.com>
2022-11-17 14:05:00 +0200

    feat: create new component - orderCardProduct

---
+++ src/components/order/orderCardProduct.tsx
@@ -0,0 +1,67 @@
+interface Props {
+  product: ({
+    id: string;
+    thumb_src: string;
+    thumb_alt: string;
+    description: string;
+    color: string;
+    title: string;
+    price: number;
+    size: string;
+    stock: boolean
+    subtotal: number;
+    shipping: number;
+    tax: number;
+  });
+  address: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export default function OrderCardProduct({
+  product,
+  address,
+  email,
+  phoneNumber
+}: Props) {
+
+  return (
+    <>
+      <div className="card shadow-xs border p-4 mb-4">
+        <div className="row border-bottom pb-4">
+          <div className="col-2">
+            <img className="w-100 rounded-3 shadow-xs" src={product.thumb_src} alt={product.thumb_alt} />
+          </div>
+          <div className="col-5">
+            <h5 className="text-base">{product.title}</h5>
+            <h6 className="text-sm font-weight-bold">${product.price.toFixed(2)}</h6>
+            <p className="text-sm mb-0 opacity-8">{product.description}</p>
+            <p className="text-dark text-sm mt-2"><b>Qty:</b> 3</p>
+          </div>
+          <div className="col-3">
+            <h5 className="text-base">Delivery Address</h5>
+            <p className="text-sm mb-0 opacity-8 pe-7">{address}</p>
+          </div>
+          <div className="col-2">
+            <h5 className="text-base">Shipping updates</h5>
+            <p className="text-sm mb-2 opacity-8">{email}</p>
+            <p className="text-sm opacity-8">{phoneNumber}</p>
+            <a className="text-sm text-primary font-weight-bold" href="#">Edit</a>
+          </div>
+        </div>
+        <div className="mt-4">
+          <h6>Preparing to ship on March 24, 2021</h6>
+          <div className="progress my-4">
+            <div className="progress-bar bg-primary w-35" role="progressbar"></div>
+          </div>
+          <div className="d-flex justify-content-between">
+            <p className="font-weight-semibold text-primary">Order placed</p>
+            <p className="font-weight-semibold text-primary">Processed</p>
+            <p className="font-weight-semibold">Shipped</p>
+            <p className="font-weight-semibold">Delivered</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
