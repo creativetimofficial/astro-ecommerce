89fe46db88fd2b90d7d025972bc7214bc8d5c7b4
Stefan <stefan@creative-tim.com>
2022-11-22 14:28:09 +0200

    refactor: update component - responsive

--- src/components/order/orderSummaries.tsx
+++ src/components/order/orderSummaries.tsx
@@ -65,23 +65,23 @@ export default function OrderSummaries({
 
   return (
     <>
-    <div className="p-5 bg-gray-100 rounded-2">
-      <div className="d-flex justify-content-between mb-4"> 
-        <div className="d-flex align-items-baseline">
+    <div className="p-3 p-md-5 bg-gray-100 rounded-2">
+      <div className="d-block d-md-flex justify-content-between mb-4"> 
+        <div className="d-block d-md-flex align-items-baseline">
           <h3 className="mb-0 me-3">Order #{order.orderNumber}</h3>
           <a className="text-sm text-primary" href="#">View invoice <i className="fas fa-long-arrow-right"></i></a>
         </div>
-        <p className="mb-0 text-dark">Order Placed <b>{order.date}</b></p>
+        <p className="mb-0 text-dark mt-4 mt-md-0">Order Placed <b>{order.date}</b></p>
       </div>
 
       {orderCards}
 
-      <div className="row bg-gray-200 p-4 mx-1 rounded-2">
-        <div className="col-3">
+      <div className="row bg-gray-200 p-2 p-md-4 mx-1 rounded-2">
+        <div className="col-12 col-lg-3 mt-4 mt-md-0">
           <h5 className="text-base">Billing Address</h5>
-          <p className="text-sm mb-0 opacity-8 pe-7">{order.address}</p>
+          <p className="text-sm mb-0 opacity-8 pe-md-7">{order.address}</p>
         </div>
-        <div className="col-3">
+        <div className="col-12 col-lg-3 mt-4 mt-lg-0">
           <h5 className="text-base">Payment information</h5>
           <div className="d-flex">
             <i className="fab fa-cc-visa text-2xl text-primary me-2 mt-1"></i>
@@ -92,7 +92,7 @@ export default function OrderSummaries({
           </div>
         </div>
         
-        <div className="col-6">
+        <div className="col-12 col-lg-6">
           <OrderSummary 
             subtotal={subtotal} 
             textColor=""  
