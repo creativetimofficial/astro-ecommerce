224353cf3fad3e5dd041e950bd20c3c0fafb8350
Stefan <stefan@creative-tim.com>
2022-11-23 18:43:39 +0200

    refactor: install Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: "server",
+  adapter: vercel()
 });
