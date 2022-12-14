d6c14424e754a0ac3c0230c3db4d7c99440ec414
Stefan <stefan@creative-tim.com>
2022-12-14 18:21:15 +0200

    feat: create new component- footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="border-top p-4">
+        <div className="copyright text-center text-sm text-muted">
+          Copyright
+          © <script>
+            document.write(new Date().getFullYear()) 
+          </script>
+          &nbsp;Astro Corporate UI by
+          <a href="https://www.creative-tim.com" className="text-secondary ms-1" target="_blank">Creative Tim</a>.
+        </div>
+      </footer>
+    </>
+  );
+}
+
+
