cbeb4173448bca852decd0993ad8bb21def098aa
Stefan <stefan@creative-tim.com>
2023-01-27 16:44:15 +0200

    refactor: update navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,8 @@
 const Navbar = () => {
-
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="./">Astro Ecommerce</a>
+        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="https://www.creative-tim.com/astro">Astro Ecommerce</a>
         <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon mt-2">
             <span className="navbar-toggler-bar bar1"></span>
@@ -14,7 +13,7 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navigation">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="https://demos.creative-tim.com/astro-ecommerce/">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="./">
                 All Components
               </a>
             </li>
@@ -23,9 +22,9 @@ const Navbar = () => {
                 Pages
               </a>
               <ul className="dropdown-menu" aria-labelledby="pagesExample">
-                <li><a className="dropdown-item" href="https://demos.creative-tim.com/astro-ecommerce/landing/">Landing Page</a></li>
-                <li><a className="dropdown-item" href="https://demos.creative-tim.com/astro-ecommerce/product/">Product Page</a></li>
-                <li><a className="dropdown-item" href="https://demos.creative-tim.com/astro-ecommerce/shopping-cart/">Shopping Cart</a></li>
+                <li><a className="dropdown-item" href="/astro-ecommerce/landing/">Landing Page</a></li>
+                <li><a className="dropdown-item" href="/astro-ecommerce/product/">Product Page</a></li>
+                <li><a className="dropdown-item" href="/astro-ecommerce/shopping-cart/">Shopping Cart</a></li>
               </ul>
             </li>
             <li className="nav-item">
