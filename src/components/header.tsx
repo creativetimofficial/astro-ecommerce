863f98926a5a5d413fe3057d88b71ba599d4d2dd
Stefan <stefan@creative-tim.com>
2023-03-22 17:21:28 +0200

    feat: create new component

---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+
+interface Props {
+  bgImage: string;
+  title: string;
+  description: string;
+}
+
+export default function PageHeader({
+  bgImage,
+  title,
+  description
+}: Props) {
+
+
+  return (
+    <>
+      <div className="page-header align-items-center min-vh-50" style={{backgroundImage: `url(${bgImage})`}}>
+        <span className="mask bg-dark opacity-8"></span>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="mx-auto text-center col-lg-7">
+              <h1 className="mb-2 text-white">{title}</h1>
+              <p className="text-white text-lead mb-4">
+                {description}
+              </p>
+              <button className="btn btn-outline-white">Explore</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
