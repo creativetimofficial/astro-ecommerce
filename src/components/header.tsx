6511f9230ef011ac7b4c816dcd70fe5175499bb8
Stefan <stefan@creative-tim.com>
2023-03-28 17:46:15 +0300

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,16 +4,22 @@ interface Props {
   title: string;
   description: string;
   filterColor: string;
+  titleColor: string;
+  descriptionColor: string;
 }
 
 export default function PageHeader({
   bgImage,
   title,
   description,
-  filterColor
+  filterColor,
+  titleColor,
+  descriptionColor
 }: Props) {
 
-  const filter = "mask opacity-8 " + filterColor;
+  const filter = "mask " + filterColor;
+  const h1Color = "mb-2 " + titleColor;
+  const pColor = "text-lead mb-4 " + descriptionColor;
 
   return (
     <>
@@ -21,9 +27,9 @@ export default function PageHeader({
         <span className={filter}></span>
         <div className="container">
           <div className="row justify-content-center">
-            <div className="mx-auto text-center col-lg-7">
-              <h1 className="mb-2 text-white">{title}</h1>
-              <p className="text-white text-lead mb-4">
+            <div className="mx-auto text-center col-lg-8">
+              <h1 className={h1Color}>{title}</h1>
+              <p className={pColor}>
                 {description}
               </p>
               <button className="btn btn-outline-white">Explore</button>
