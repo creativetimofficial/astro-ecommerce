31b56a1b0d2d09129f51a1726616ed441307a6c8
Stefan <stefan@creative-tim.com>
2023-03-30 01:24:48 +0300

    refactor: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce',
+  base: '/astro-ecommerce'
 });
