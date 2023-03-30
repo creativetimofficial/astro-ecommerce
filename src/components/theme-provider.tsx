dab3cb7050440c51f807b706dcefa158f260ce78
Stefan <stefan@creative-tim.com>
2023-03-30 15:21:36 +0300

    refactor: add custom theme for inputs, navbar and iconbutton

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -61,6 +61,133 @@ const customTheme = {
       },
     },
   },
+  iconButton: {
+    valid: {
+      colors: [
+        'dark'
+      ],
+    },
+    styles: {
+      variants: {
+        outlined: {
+          cyan: {
+            background: "bg-cyan",
+            color: "text-dark"
+          },
+          yellow: {
+            background: "bg-yellow",
+            color: "text-dark"
+          },
+          blue: {
+            background: "bg-blue",
+            color: "text-white"
+          },
+          red: {
+            background: "bg-red",
+            color: "text-white"
+          },
+          dark: {
+            background: "bg-dark",
+            color: "text-white"
+          },
+        }
+      },
+
+    },
+  },
+  navbar: {
+    styles: {
+      variants: {
+        filled: {
+          cyan: {
+            background: "bg-cyan",
+            color: "text-dark"
+          },
+          yellow: {
+            background: "bg-yellow",
+            color: "text-dark"
+          },
+          blue: {
+            background: "bg-blue",
+            color: "text-white"
+          },
+          red: {
+            background: "bg-red",
+            color: "text-white"
+          },
+          dark: {
+            background: "bg-dark",
+            color: "text-white"
+          }
+        }
+      },
+    },
+    valid: {
+      colors: [
+        'dark'
+      ],
+    }
+  },
+
+  card: {
+    valid: {
+      colors: [
+        'dark'
+      ],
+    },
+    styles: {
+      variants: {
+        filled: {
+          cyan: {
+            background: "bg-cyan"
+          },
+          yellow: {
+            background: "bg-yellow"
+          },
+          blue: {
+            background: "bg-blue"
+          },
+          red: {
+            background: "bg-red"
+          },
+          dark: {
+            background: "bg-dark"
+          },
+
+        }
+      }
+    }
+  },
+
+  input: {
+    styles: {
+      base: {
+     
+      },
+      variants: {
+        outlined: {
+          cyan: {
+            background: "bg-cyan"
+          },
+          yellow: {
+            background: "bg-yellow"
+          },
+          blue: {
+            background: "bg-blue"
+          },
+          red: {
+            background: "bg-red"
+          },
+          dark: {
+            background: "bg-dark",
+            className: "border border-[#383B3D]"
+
+          },
+
+        }
+      }
+    }
+  }
 };
 
 export function ThemeProvider({ children }: any) {
