2ad48602fc74b3c24ebb356d1076b43167912cee
Stefan <stefan@creative-tim.com>
2023-04-10 22:11:57 +0300

    feat: add new section - categories

---
+++ src/components/landing/categories.tsx
@@ -0,0 +1,89 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  IconButton,
+  Typography,
+} from "@material-tailwind/react";
+
+interface CategoriesCardPropsType {
+  img: string;
+  name: string;
+  title: string;
+  desc: string;
+}
+
+function CategoriesCard({ img, category }: CategoriesCardPropsType) {
+  return (
+    <Card
+      shadow={false}
+      className="relative grid h-[25rem] w-full max-w-[28rem] items-end justify-center overflow-hidden text-center"
+    >
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="absolute inset-0 m-0 h-full w-full rounded-none bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-center"
+      >
+        <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
+      </CardHeader>
+      <CardBody className="relative py-14 px-6 md:px-12">
+        <Typography
+          variant="h4"
+          color="white"
+          className="mb-6 font-medium leading-[1.5]"
+        >
+          {category}
+        </Typography>
+      </CardBody>
+    </Card>
+  );
+}
+
+const categories = [
+  {
+    img: "https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    category: "Electronics",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1541643600914-78b084683601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    category: "New Arrivals",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1504274066651-8d31a536b11a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    category: "Accessories",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1556217256-f4e659e15103?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    category: "Sale",
+  },
+];
+
+export function TeamSectionOne() {
+  return (
+    <section className="py-8 px-8 lg:py-16">
+      <div className="container mx-auto">
+        <div className="mb-24 w-1/2">
+          <Typography variant="h2" color="blue-gray" className="mb-4">
+            Shop by category
+          </Typography>
+          <Typography
+            variant="lead"
+            color="gray"
+            className="l:px-20"
+          >
+            This is the paragraph where you can write more details about your
+            categories. Keep you user engaged by providing meaningful information.
+          </Typography>
+        </div>
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {categories.map((props, key) => (
+            <CategoriesCard key={key} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TeamSectionOne;
