0dd4ee225b427c5f1fccd8014a34c2988250ae64
Stefan <stefan@creative-tim.com>
2023-04-12 10:56:43 +0300

    refactor: update navbar - billing page

--- src/components/billing/header.tsx
+++ src/components/billing/header.tsx
@@ -4,35 +4,17 @@ import {
   TabsHeader,
   Tab,
   Typography,
-  Navbar,
-  MobileNav,
-  IconButton,
   Button,
   Card,
   CardBody,
 } from "@material-tailwind/react";
 import ThemeProvider from "../theme-provider";
+import Navbar from "../defaultNavbar"
 
-import { Bars3Icon, XMarkIcon, CheckCircleIcon } from "@heroicons/react/24/outline";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
 interface NavItemPropsType {
   children: React.ReactNode;
-}
-
-function NavItem({ children }: NavItemPropsType) {
-  return (
-    <li>
-      <Typography
-        as="a"
-        href="#"
-        variant="small"
-        color="gray"
-        className="font-normal transition-colors hover:text-gray-900"
-      >
-        {children}
-      </Typography>
-    </li>
-  );
 }
 
 function PricingCard({ price, desc, options }) {
@@ -82,15 +64,6 @@ function PricingCard({ price, desc, options }) {
 
 export function PricingSectionOne() {
   const [type, setType] = React.useState("monthly");
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen((cur) => !cur);
-
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpen(false)
-    );
-  }, []);
 
   const cards = [
     {
@@ -130,60 +103,7 @@ export function PricingSectionOne() {
 
   return (
     <ThemeProvider>
-      <Navbar
-        className="absolute z-50 border-0"
-        shadow={false}
-        fullWidth
-      >
-        <div className="container mx-auto flex items-center">
-          <Typography color="blue-gray" className="!mb-0 !font-bold">
-            Astro Ecommerce
-          </Typography>
-          <ul className="ml-10 hidden items-center gap-6 lg:flex">
-            <NavItem>Home</NavItem>
-            <NavItem>About</NavItem>
-            <NavItem>Resources</NavItem>
-            <NavItem>Pricing</NavItem>
-          </ul>
-          <div className="ml-auto hidden gap-2 lg:flex">
-            <Button variant="text" size="sm">
-              login
-            </Button>
-            <Button size="sm">sign up</Button>
-          </div>
-          <IconButton
-            size="sm"
-            variant="text"
-            color="blue-gray"
-            onClick={handleOpen}
-            className="ml-auto inline-block lg:hidden"
-          >
-            {open ? (
-              <XMarkIcon strokeWidth={2} className="h-6 w-6" />
-            ) : (
-              <Bars3Icon strokeWidth={2} className="h-6 w-6" />
-            )}
-          </IconButton>
-        </div>
-        <MobileNav open={open}>
-          <div className="container mx-auto mt-3 border-t border-blue-gray-50 px-2 pt-4">
-            <ul className="flex flex-col gap-4">
-              <NavItem>Home</NavItem>
-              <NavItem>About</NavItem>
-              <NavItem>Resources</NavItem>
-              <NavItem>Pricing</NavItem>
-            </ul>
-            <div className="mt-8 mb-4 flex gap-2">
-              <Button variant="outlined" size="sm" fullWidth>
-                login
-              </Button>
-              <Button color="dark" size="sm" fullWidth>
-                sign up
-              </Button>
-            </div>
-          </div>
-        </MobileNav>
-      </Navbar>
+      <Navbar />
       <section>
         <div className="min-h-[50vh] px-8 py-24 flex align-center flex-col justify-center text-center bg-cover" style={{backgroundImage: `url('./bg-billing.jpg')`}}>
           <div className="container mx-auto text-center">
