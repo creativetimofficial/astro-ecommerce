09cbe95943525251428011cd809bbef55f150d8a
Stefan <stefan@creative-tim.com>
2023-04-12 10:56:36 +0300

    refactor: update navbar - about page

--- src/components/about/header.tsx
+++ src/components/about/header.tsx
@@ -1,106 +1,24 @@
 import React from "react";
 import {
-  Navbar,
-  MobileNav,
+
+
   Button,
-  IconButton,
+
   Typography,
 } from "@material-tailwind/react";
 import ThemeProvider from "../theme-provider";
-
+import Navbar from "../navbar";
 import {
   Bars3Icon,
   XMarkIcon,
   ArrowSmallRightIcon,
 } from "@heroicons/react/24/outline";
 
-interface NavItemPropsType {
-  children: React.ReactNode;
-}
-
-function NavItem({ children }: NavItemPropsType) {
-  return (
-    <li>
-      <Typography
-        as="a"
-        href="#"
-        variant="small"
-        color="gray"
-        className="font-normal transition-colors hover:text-gray-900"
-      >
-        {children}
-      </Typography>
-    </li>
-  );
-}
-
 export function HeroSectionTwo() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen((cur) => !cur);
-
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpen(false)
-    );
-  }, []);
-
+ 
   return (
     <ThemeProvider>
-      <Navbar
-        className="fixed z-50 border-x-0 border-t-0 !border-b !border-blue-gray-50"
-        shadow={false}
-        fullWidth
-      >
-        <div className="container mx-auto flex items-center">
-          <Typography color="blue-gray" className="!mb-0 !font-bold">
-            Astro Ecommerce
-          </Typography>
-          <ul className="ml-10 hidden items-center gap-6 lg:flex">
-            <NavItem>Home</NavItem>
-            <NavItem>About</NavItem>
-            <NavItem>Resources</NavItem>
-            <NavItem>Pricing</NavItem>
-          </ul>
-          <div className="ml-auto hidden gap-2 lg:flex">
-            <Button variant="text" size="sm">
-              login
-            </Button>
-            <Button size="sm">sign up</Button>
-          </div>
-          <IconButton
-            size="sm"
-            variant="text"
-            color="blue-gray"
-            onClick={handleOpen}
-            className="ml-auto inline-block lg:hidden"
-          >
-            {open ? (
-              <XMarkIcon strokeWidth={2} className="h-6 w-6" />
-            ) : (
-              <Bars3Icon strokeWidth={2} className="h-6 w-6" />
-            )}
-          </IconButton>
-        </div>
-        <MobileNav open={open}>
-          <div className="container mx-auto mt-3 border-t border-blue-gray-50 px-2 pt-4">
-            <ul className="flex flex-col gap-4">
-              <NavItem>Home</NavItem>
-              <NavItem>About</NavItem>
-              <NavItem>Resources</NavItem>
-              <NavItem>Pricing</NavItem>
-            </ul>
-            <div className="mt-8 mb-4 flex gap-2">
-              <Button variant="outlined" size="sm" fullWidth>
-                login
-              </Button>
-              <Button size="sm" fullWidth>
-                sign up
-              </Button>
-            </div>
-          </div>
-        </MobileNav>
-      </Navbar>
+      <Navbar />
       <header className="h-full w-screen place-items-center bg-white px-8 py-28">
         <div className="container mx-auto grid items-center lg:grid-cols-2">
           <div className="text-center lg:text-left">
@@ -154,8 +72,8 @@ export function HeroSectionTwo() {
               </Button>
             </div>
           </div>
-          <div className="hidden justify-center lg:flex">
-            <img src="https://images.unsplash.com/photo-1650615567023-0721bceeecb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80" alt="iphone" className="max-w-md rounded-3xl" />
+          <div className="hidden lg:flex">
+            <img src="https://images.unsplash.com/photo-1650615567023-0721bceeecb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80" alt="iphone" className="max-w-md rounded-3xl ml-auto" />
           </div>
         </div>
       </header>
