e68fd7a3c4996417e8c21f5ee5e2ffc02cf2fdd3
Stefan <stefan@creative-tim.com>
2023-04-12 10:58:33 +0300

    refactor: update navbar - signup page

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,97 +1,12 @@
-import React from "react";
 import ThemeProvider from "./theme-provider";
+import Navbar from "./defaultNavbar"
  
-import { Navbar, Typography, IconButton, Input, MobileNav, Checkbox, Button } from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
-
-interface NavItemPropsType {
-  children: React.ReactNode;
-}
-
-function NavItem({ children }: NavItemPropsType) {
-  return (
-    <li>
-      <Typography
-        as="a"
-        href="#"
-        variant="small"
-        color="gray"
-        className="font-normal transition-colors hover:text-gray-900"
-      >
-        {children}
-      </Typography>
-    </li>
-  );
-}
+import { Typography, Input, Button } from "@material-tailwind/react";
 
 export function SignUp() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen((cur) => !cur);
-
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpen(false)
-    );
-  }, []);
-
   return (
     <ThemeProvider>
-      <Navbar
-        className="absolute z-50 border-x-0 border-t-0 !border-b !border-blue-gray-50"
-        shadow={false}
-        fullWidth
-      >
-        <div className="container mx-auto flex items-center">
-          <Typography color="blue-gray" className="!mb-0 !font-bold">
-            Astro Ecommerce
-          </Typography>
-          <ul className="ml-10 hidden items-center gap-6 lg:flex">
-            <NavItem>Home</NavItem>
-            <NavItem>About</NavItem>
-            <NavItem>Resources</NavItem>
-            <NavItem>Pricing</NavItem>
-          </ul>
-          <div className="ml-auto hidden gap-2 lg:flex">
-            <Button variant="text" size="sm">
-              login
-            </Button>
-            <Button size="sm">sign up</Button>
-          </div>
-          <IconButton
-            size="sm"
-            variant="text"
-            color="blue-gray"
-            onClick={handleOpen}
-            className="ml-auto inline-block lg:hidden"
-          >
-            {open ? (
-              <XMarkIcon strokeWidth={2} className="h-6 w-6" />
-            ) : (
-              <Bars3Icon strokeWidth={2} className="h-6 w-6" />
-            )}
-          </IconButton>
-        </div>
-        <MobileNav open={open}>
-          <div className="container mx-auto mt-3 border-t border-blue-gray-50 px-2 pt-4">
-            <ul className="flex flex-col gap-4">
-              <NavItem>Home</NavItem>
-              <NavItem>About</NavItem>
-              <NavItem>Resources</NavItem>
-              <NavItem>Pricing</NavItem>
-            </ul>
-            <div className="mt-8 mb-4 flex gap-2">
-              <Button variant="outlined" size="sm" fullWidth>
-                login
-              </Button>
-              <Button size="sm" fullWidth>
-                sign up
-              </Button>
-            </div>
-          </div>
-        </MobileNav>
-      </Navbar>
+      <Navbar />
       <section className="grid h-screen items-center p-8">
         <div className="text-center">
           <Typography variant="h3" color="blue-gray" className="mb-2">
