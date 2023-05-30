9bb0cc3bf42d666b3d9efa41c424532df736207c
Stefan <stefan@creative-tim.com>
2023-05-30 12:39:12 +0300

    refactor: update faq - billing page

--- src/components/billing/faq.tsx
+++ src/components/billing/faq.tsx
@@ -33,6 +33,23 @@ const faqs = [
   },
 ];
 
+function Icon({ id, open }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+    </svg>
+  );
+}
+
 export function Faqs() {
   const [open, setOpen] = React.useState(0);
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
@@ -55,8 +72,9 @@ export function Faqs() {
               key={key}
               open={open === key + 1}
               onClick={() => handleOpen(key + 1)}
+              icon={<Icon id={key + 1} open={open} />}
             >
-              <AccordionHeader className="text-left">{title}</AccordionHeader>
+              <AccordionHeader className="text-left" onClick={() => handleOpen(1)}>{title}</AccordionHeader>
               <AccordionBody>
                 <Typography
                   color="blue-gray"
