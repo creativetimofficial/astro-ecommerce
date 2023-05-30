6d2a80aea47ed019a4e36ac68a46acad9e5da0b3
Stefan <stefan@creative-tim.com>
2023-05-30 11:28:49 +0300

    feat: create new component - speedDial

---
+++ src/components/dashboard/speedDial.tsx
@@ -0,0 +1,38 @@
+import {
+  IconButton,
+  SpeedDial,
+  SpeedDialHandler,
+  SpeedDialContent,
+  SpeedDialAction,
+} from "@material-tailwind/react";
+import {
+  PlusIcon,
+  HomeIcon,
+  CogIcon,
+  Square3Stack3DIcon,
+} from "@heroicons/react/24/outline";
+ 
+export default function DefaultSpeedDial() {
+  return (
+      <div className="absolute bottom-5 right-5">
+        <SpeedDial>
+          <SpeedDialHandler>
+            <IconButton size="lg" className="rounded-full">
+              <PlusIcon className="h-5 w-5 transition-transform group-hover:rotate-45" />
+            </IconButton>
+          </SpeedDialHandler>
+          <SpeedDialContent>
+            <SpeedDialAction>
+              <HomeIcon className="h-5 w-5" />
+            </SpeedDialAction>
+            <SpeedDialAction>
+              <CogIcon className="h-5 w-5" />
+            </SpeedDialAction>
+            <SpeedDialAction>
+              <Square3Stack3DIcon className="h-5 w-5" />
+            </SpeedDialAction>
+          </SpeedDialContent>
+        </SpeedDial>
+      </div>
+  );
+}
