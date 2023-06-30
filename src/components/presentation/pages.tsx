27b45ecc628258c156d8097f638d28e3235523a6
Stefan <stefan@creative-tim.com>
2023-06-30 11:25:22 +0300

    refactor: update pages section - responsive

--- src/components/presentation/pages.tsx
+++ src/components/presentation/pages.tsx
@@ -33,13 +33,6 @@ function BlogPostCard({
         <img src={img} alt={title} className="h-full w-full object-cover" />
       </CardHeader>
       <CardBody className="p-0">
-        <Typography
-          variant="small"
-          color="blue"
-          className="mb-3 !font-semibold"
-        >
-          {author}&nbsp;&bull;&nbsp;{date}
-        </Typography>
         <a
           href="#"
           className="grid grid-cols-6 items-baseline gap-2 text-blue-gray-900 transition-colors hover:text-blue-500"
@@ -49,7 +42,6 @@ function BlogPostCard({
               {title}
             </Typography>
           </div>
-          <ArrowUpRightIcon className="ml-auto h-5 w-5" strokeWidth={3} />
         </a>
         <Typography color="gray" className="mb-6 font-normal">
           {desc}
@@ -63,22 +55,16 @@ function BlogPostCard({
 const posts = [
   {
     img: "https://images.unsplash.com/photo-1486728297118-82a07bc48a28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2429&q=80",
-    author: "Alec Whitten",
-    date: "17 Jan 2022",
     title: "Search and Discover",
     desc: "As we live, our hearts turn colder. Cause pain is what we go through as we become older. We get insulted by others.",
   },
   {
     img: "https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80",
-    author: "Demi Wilkinson",
-    date: "16 Jan 2022",
     title: "US investment ticks up",
     desc: "Venture investment in U.S. startups rose sequentially in the second quarter of 2022, boosted by large, ate-stage financings.",
   },
   {
     img: "https://images.unsplash.com/photo-1548611716-3000815a5803?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGRlc2slMjBzZXR1cHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-    author: "Candice Wu",
-    date: "15 Jan 2022",
     title: "Web3 startups $5M",
     desc: "Insticator is announcing that it has raised $5.2 million in Series A funding. The startup allows online publishers to add quizzes.",
   },
@@ -90,7 +76,7 @@ export function BlogSectionThree() {
       <section>
         <div className="container mx-auto px-8 py-10">
           <div className="mb-16 text-left">
-            <Typography color="blue" className="mb-2 !font-semibold">
+            <Typography color="dark" className="mb-2 text-lg !font-semibold">
               Get inspired!
             </Typography>
             <Typography variant="h2" color="blue-gray">
