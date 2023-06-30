6833912e30064cf1b6d147475454522d7b85806a
Stefan <stefan@creative-tim.com>
2023-06-30 11:25:10 +0300

    refactor: update header section - responsive

--- src/components/presentation/header.tsx
+++ src/components/presentation/header.tsx
@@ -17,10 +17,10 @@ export function HeroPresentation() {
       <header className="h-full w-screen place-items-center bg-white px-8 py-8">
         <div className="container mx-auto grid items-center lg:grid-cols-2">
           <div className="text-center lg:text-left">
-            <div className="mb-8 inline-flex items-center rounded-full bg-dark/30 py-1 pl-1 pr-3">
+            <div className="mb-8 inline-flex items-center rounded-lg border border-dark/30 py-1 pl-1 pr-3">
               <Typography
                 variant="small"
-                className="mr-3 rounded-full bg-white py-0.5 px-3 font-medium text-dark-500"
+                className="mr-3 rounded-md bg-dark py-0.5 px-3 font-medium text-white"
               >
                 New
               </Typography>
@@ -29,7 +29,7 @@ export function HeroPresentation() {
                 variant="small"
                 className="!flex !items-center !font-semibold"
               >
-                ASTRO STARTER TEMPLATE
+                Astro Starter Template
                 <ArrowSmallRightIcon
                   className="ml-1.5 h-4 w-4"
                   strokeWidth={3}
@@ -41,21 +41,16 @@ export function HeroPresentation() {
               color="blue-gray"
               className="mb-8 leading-tight lg:text-5xl"
             >
-              The perfect starting point for your <span className="text-blue">Astro Ecommerce</span>
+              The perfect starting point for your Astro Ecommerce
             </Typography>
             <Typography variant="lead" color="gray" className="lg:pr-20">
-            Astro Ecommerce is a free template designed to be both lightweight and feature-rich. It comes packed with everything you need to get your Online Store ready in no time.
+              Astro Ecommerce is a free template designed to be both lightweight and feature-rich. It comes packed with everything you need to get your Online Store ready in no time.
             </Typography>
             <div className="mt-12 flex flex-wrap justify-center gap-3 lg:justify-start">
-              <Button color="dark" className="flex items-center">
-                <img
-                  src=".//logos/logo-ios.svg"
-                  alt="ios"
-                  className="-mt-1 mr-1.5 h-6 w-6"
-                />
+              <Button color="dark" variant="outlined" className="flex items-center">
                 Get Started
               </Button>
-              <Button color="blue" className="flex items-center">
+              <Button color="dark" className="flex items-center">
                 <img
                   src=".//logos/logo-android.svg"
                   alt="ios"
