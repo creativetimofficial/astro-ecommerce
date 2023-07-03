860e833f5cec7c6a01cf7d4deacdc6677f1d3453
Stefan <stefan@creative-tim.com>
2023-07-03 19:23:22 +0300

    refactor: update header billing page

--- src/components/billing/header.tsx
+++ src/components/billing/header.tsx
@@ -11,8 +11,6 @@ import {
 import ThemeProvider from "../theme-provider";
 import Navbar from "../defaultNavbar"
 
-import { CheckCircleIcon } from "@heroicons/react/24/outline";
-
 interface NavItemPropsType {
   children: React.ReactNode;
 }
@@ -43,10 +41,9 @@ function PricingCard({ price, desc, options }) {
           <ul className="my-8 flex flex-col gap-3 border-y border-blue-gray-50 py-6">
             {options.map((option, key) => (
               <li key={key} className="flex items-center gap-2">
-                <CheckCircleIcon
-                  className="h-6 w-6 text-blue-500"
-                  strokeWidth={2}
-                />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-dark">
+                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+                </svg>
                 <Typography color="gray" className="font-normal opacity-80">
                   {option}
                 </Typography>
@@ -136,7 +133,7 @@ export function PricingSectionOne() {
             </Tabs>
           </div>
         </div>
-        <div className="-mt-32 px-8 pt-8 pb-16">
+        <div className="-mt-16 px-8 pt-8 pb-16">
           <div className="container mx-auto grid gap-10 md:grid-cols-2 lg:grid-cols-3">
             {cards.map((props, key) => (
               <PricingCard key={key} {...props} />
