aa08ed9fb91df0452b8acd7d1c5bacee5054c209
Stefan <stefan@creative-tim.com>
2023-07-04 17:31:43 +0300

    Update resetPassword.tsx

--- src/components/resetPassword.tsx
+++ src/components/resetPassword.tsx
@@ -24,18 +24,14 @@ export function Reset() {
         </div>
         <div className="container mx-auto -mt-32 grid justify-center">
           <Card className="mx-4 md:w-[26rem]">
-            <CardHeader
-              variant="gradient"
-              color="dark"
-              className="mb-4 grid place-items-center py-8 px-4 text-center bg-dark"
-            >
-              <Typography variant="h3" color="white" className="mb-2">
+            <div className="py-8 px-4 text-center">
+              <Typography variant="h3" color="dark" className="mb-2">
                 Reset Password
               </Typography>
-              <Typography variant="small" color="white" className="font-normal">
+              <Typography variant="small" color="blue-gray" className="font-normal">
                 You will receive an e-mail in maximum 60 seconds
               </Typography>
-            </CardHeader>
+            </div>
             <CardBody className="flex flex-col gap-4">
               <Input
                 variant="static"
