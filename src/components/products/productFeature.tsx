365665ecf3cb19a7eb14715a97ad9be00db123bf
Stefan <stefan@creative-tim.com>
2023-07-18 16:13:47 +0300

    refactor: update product feature

--- src/components/products/productFeature.tsx
+++ src/components/products/productFeature.tsx
@@ -21,16 +21,21 @@ export default function ProductOverview({
   return (
     <>
     <div className="card card-product card-plain">
-      <div className="row mt-5">
-        <div className="col-12 col-lg-6">
+      <div className="row">
+        <div className="col-12 col-lg-7 mx-auto text-center">
           {(title.length != 0) && 
             <h2 className="mb-3">{title}</h2>
           }
           {(full_description.length != 0) && 
             <p className="mb-5">{full_description}</p>
           }
-          
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col-12 col-lg-6 pe-5">
           <div className="row">
+            <h4 className="mb-3">Product Description</h4>
+            <p className="mb-5">Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old.</p>
             <ProductFeatureDetails featuresDetails={featuresDetails} />
           </div>
         </div>
