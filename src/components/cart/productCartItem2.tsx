708d371377575a37643ba9eb09f005e28f9ac5af
Stefan <stefan@creative-tim.com>
2023-07-19 18:00:04 +0300

    Update productCartItem2.tsx

--- src/components/cart/productCartItem2.tsx
+++ src/components/cart/productCartItem2.tsx
@@ -22,32 +22,37 @@ export default function CartItem({
   return (
     <>
       <div className="d-block d-md-flex mt-4">
-        <img className="w-50 w-md-20 w-lg-10 rounded-3 shadow-xs m-4" src={thumb_src} alt={thumb_alt} />
+        <img className="w-50 w-md-20 w-lg-10 rounded-3 m-4" src={thumb_src} alt={thumb_alt} />
         <div className="w-100 w-md-50 pt-4 ps-md-4">
-          <h6 className="text-lg mb-1">{title}</h6>
-            <p className="mb-0">{color}</p>
-            <p className="mb-0">{size}</p>
+          <div className="d-flex align-items-center mb-2">
+            <h6 className="text-lg mb-0">{title}</h6>
 
-          <div className="d-flex align-items-center mt-6">
-           {(stock) ? 
-            <>
-              <i className="fas fa-check text-lg text-success"></i>
-              <p className="mb-0 ms-2 text-sm">In Stock</p>
-            </>
-            :
-            <>
-              <i className="fas fa-minus-circle text-lg"></i>
-              <p className="mb-0 ms-2 text-sm">Out of Stock</p>
-            </>
-           }
+            <div className="d-flex align-items-center ms-3">
+              {(stock) ? 
+                <>
+                  <i className="fas fa-check text-lg text-success"></i>
+                  <p className="mb-0 ms-2 text-sm">In Stock</p>
+                </>
+                :
+                <>
+                  <i className="fas fa-minus-circle text-lg"></i>
+                  <p className="mb-0 ms-2 text-sm">Out of Stock</p>
+                </>
+              }
+            </div>
           </div>
+          <div className="d-flex align-items-center">
+            <p className="mb-0">{color}</p>
+            <span className="me-2">,</span>
+            <p className="mb-0">{size}</p>
+          </div>
+          <h6 className="mb-1 mt-5">${price.toLocaleString()}</h6>
         </div>
-        <div className="w-20 w-md-5 pt-4">
+        <div className="w-40 w-md-5 pt-4 ms-auto me-4">
           <input type="number" min={0} className="form-control mb-3" placeholder="1" aria-label="amount" />
-          <a className="text-primary" href="#">Remove</a>
         </div>
-        <div className="w-25 pt-4 text-end">
-          <h6 className="mb-1">${price.toFixed(2)}</h6>
+        <div className="w-5 pt-4 mt-1 text-end">
+          <a className="text-dark" href="#">Remove</a>
         </div>
       </div>
     </>
