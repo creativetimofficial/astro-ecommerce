15452b8c6aa7abc4a2503c4131a4e49f7f753d4f
Stefan <stefan@creative-tim.com>
2023-07-19 18:00:15 +0300

    Update productGallery.tsx

--- src/components/products/productGallery.tsx
+++ src/components/products/productGallery.tsx
@@ -11,18 +11,15 @@ export default function ProductGallery({
 
   return (
     <>
-      <div className="row">
-        <div className="col-4">
-          <img className="w-100 rounded-2" src={images[0].src} alt={images[0].alt} />
+      <div className="col-12 col-lg-6 d-flex">
+        <div className="d-block">
+          <img className="w-90 max-height-150 mb-4 rounded-2" src={images[0].src} alt={images[0].alt} />
+          <img className="w-90 max-height-150 mb-4 rounded-2" src={images[1].src} alt={images[1].alt} />
+          <img className="w-90 max-height-150 mb-4 rounded-2" src={images[2].src} alt={images[2].alt} />
+          <img className="w-90 max-height-150 rounded-2" src={images[3].src} alt={images[3].alt} />
         </div>
-        <div className="col-4">
-          <img className="w-100 rounded-2" src={images[1].src} alt={images[1].alt} />
-          <img className="w-100 mt-4 rounded-2" src={images[2].src} alt={images[2].alt} />
-        </div>
-        <div className="col-4">
-          <img className="w-100 rounded-2" src={images[3].src} alt={images[3].alt} />
-        </div>
-      </div>  
+        <img className="w-70 rounded-2" src={images[0].src} alt={images[0].alt} />
+      </div> 
     </>
   );
 }
