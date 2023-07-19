b4d0addd7ab918271a3091accf2163131361459f
Stefan <stefan@creative-tim.com>
2023-07-19 18:00:10 +0300

    Update shoppingCart2.tsx

--- src/components/cart/shoppingCart2.tsx
+++ src/components/cart/shoppingCart2.tsx
@@ -27,7 +27,8 @@ export default function ShoppingCart({
   return (
     <>
       <div className="container">
-        <h2 className="mb-4">Shopping Cart</h2>
+        <h2 className="mb-3 text-center">Shopping Cart</h2>
+        <h5 className="text-center mb-5">You are eligible for Free Shipping.</h5>
         <div className="row">
           <div className="col-12">
           {products.map((product, i) => 
@@ -47,7 +48,7 @@ export default function ShoppingCart({
               </>
             )}
           </div>
-          <div className="col-12 col-md-7 ms-auto mt-4">
+          <div className="col-12 col-md-7 mx-auto mt-4">
             <div className="card shadow-xs border bg-gray-100">
               <div className="card-body p-lg-5">
                 <OrderSummary 
@@ -55,8 +56,10 @@ export default function ShoppingCart({
                 />
               </div>
             </div>
-            <button className="btn btn-primary btn-lg w-100 mt-4">Checkout</button>
-            <p className="text-center mb-0">or <a className="text-primary ms-2" href="#">continue shopping <i className="fas fa-arrow-right ms-1"></i></a></p>
+            <div className="d-flex">
+              <button className="btn btn-white btn-lg w-100 mt-4 me-4">Countinue Shopping</button>
+              <button className="btn btn-dark btn-lg w-100 mt-4">Checkout</button>
+            </div>
           </div>
         </div>
       </div>
