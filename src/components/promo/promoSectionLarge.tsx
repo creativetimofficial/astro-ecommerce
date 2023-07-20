b3a48ad8e403fc60fe46e58a6e3f06d11dcfcf9a
Stefan <stefan@creative-tim.com>
2023-07-20 21:02:31 +0300

    Update promoSectionLarge.tsx

--- src/components/promo/promoSectionLarge.tsx
+++ src/components/promo/promoSectionLarge.tsx
@@ -26,13 +26,13 @@ export default function TestimonialsFade({
     <>
       <section className="mb-8">
         <div className="page-header py-5 py-md-0" style={styles.pageHeader}>
-          <span className="mask bg-black opacity-4"></span>
+          <span className="mask bg-black opacity-6"></span>
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-8 col-sm-9 text-center mx-auto">
                 <h1 className="text-white mb-4">{title}</h1>
                 <p className="lead text-white mb-sm-6 mb-4">{full_description}</p>
-                <button className="btn btn-white btn-lg">Explore</button>
+                <button className="btn btn-white btn-lg">Explore New Collection</button>
               </div>
             </div>
           </div>
