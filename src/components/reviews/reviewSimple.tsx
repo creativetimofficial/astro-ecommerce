11c6bf8b15f5fd06c8855e0dc0378a962489ee27
Stefan <stefan@creative-tim.com>
2023-07-20 21:02:37 +0300

    Update reviewSimple.tsx

--- src/components/reviews/reviewSimple.tsx
+++ src/components/reviews/reviewSimple.tsx
@@ -24,10 +24,13 @@ export default function ReviewSimple({
   });
   return (
     <>
-    <div className="w-100 w-md-80 w-lg-50 mx-auto">
-      {CommentsList}
-    </div>
-
+      <div className="mx-auto text-center w-md-60 mb-5">
+        <h3>Our Customer’s Opinion</h3>
+        <p>Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. </p>
+      </div>
+      <div className="w-100 w-md-80 w-lg-60 mx-auto">
+        {CommentsList}
+      </div>
     </>
     
   );
