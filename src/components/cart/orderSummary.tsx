9b8e35d2e8bfa6855b05a7e50ae0e6239330656c
Stefan <stefan@creative-tim.com>
2023-07-20 21:01:31 +0300

    Update orderSummary.tsx

--- src/components/cart/orderSummary.tsx
+++ src/components/cart/orderSummary.tsx
@@ -21,19 +21,19 @@ export default function OrderSummary({
   return (
     <>
       <ul className="list-unstyled">
-        <li className="border-bottom mt-3">
+        <li className="mt-2">
           <div className="d-flex justify-content-between">
             <p className={`opacity-8` + variant}>Subtotal</p>
             <p className={`fw-bold opacity-8` + variant}>${sum.toLocaleString()}</p>
           </div>
         </li>
-        <li className="border-bottom mt-3">
+        <li className="mt-2">
           <div className="d-flex justify-content-between">
             <p className={`opacity-8` + variant}>Shipping estimate <span data-bs-toggle="tooltip" data-bs-placement="top" title="More information related to shipping" data-container="body" data-animation="true"><i className="fas fa-question-circle text-sm"></i></span></p>
             <p className={`fw-bold opacity-8` + variant}>${shipping.toLocaleString()}</p>
           </div>
         </li>
-        <li className="border-bottom mt-3">
+        <li className="border-bottom mt-2">
           <div className="d-flex justify-content-between">
             <p className={`opacity-8` + variant}>Tax estimate <span data-bs-toggle="tooltip" data-bs-placement="top" title="This may vary depending on the country you are in" data-container="body" data-animation="true"><i className="fas fa-question-circle text-sm"></i></span></p>
             <p className={`fw-bold opacity-8` + variant}>${tax.toLocaleString()}</p>
