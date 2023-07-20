5201b795ba50b50e663231e4f7cd6ac029ddf068
Stefan <stefan@creative-tim.com>
2023-07-20 21:02:40 +0300

    Update reviewSummaryChart.tsx

--- src/components/reviews/reviewSummaryChart.tsx
+++ src/components/reviews/reviewSummaryChart.tsx
@@ -31,25 +31,28 @@ export default function ReviewSummaryChart({
 
   return (
     <>
+    <div className="mx-auto text-center w-md-60 mb-5">
+      <h3>Our Customer’s Opinion</h3>
+      <p>Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. </p>
+    </div>
     <div className="row">
       <div className="col-12 col-lg-5">
-        <div className="card shadow-xs mb-4">
-          <div className="card-body p-md-5">
-          <h4 className="mb-3">Customer Reviews</h4>
-          <div className="d-flex">
+        <div className="p-4 mb-4">
+          <h4 className="mb-3">Client Reviews</h4>
+          <div className="d-flex align-items-center">
+            <h6 className="mb-0 me-2">4.7</h6>
             <ReviewRating rating={rating}/>
             <p className="mb-0 ms-2">Based on <b>{reviews.length}</b> reviews</p> 
           </div>
-          <div className="my-4">
+          <div className="mt-4 mb-5">
             <ReviewProgress reviews={reviews}/>
           </div>
-          <h6>Share your thoughts</h6>
-          <p>If you have used this product, share your thoughts with other customers.</p>
-          <button className="btn btn-white btn-lg w-100 mb-0">Write a review</button>
-          </div>
+          <h5>We value your opinion</h5>
+          <p className="mb-5">The time is now for it to be okay to be great. People in this world shun people for being great. </p>
+          <button className="btn btn-dark w-100 mb-0">Write a review</button>
         </div>
       </div>
-      <div className="col-12 col-lg-7 max-height-500 overflow-scroll shadow-xs rounded-3 pt-4">
+      <div className="col-12 col-lg-7 max-height-500 overflow-scroll rounded-3 pt-4">
         <div className="card card-plain">
           <div className="card-body">
             {CommentsList}
