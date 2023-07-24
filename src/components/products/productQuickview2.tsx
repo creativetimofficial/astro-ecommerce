87d24d0df042746dbcabb6154cae30d674ec52b7
Stefan <stefan@creative-tim.com>
2023-07-24 18:57:19 +0300

    Update productQuickview2.tsx

--- src/components/products/productQuickview2.tsx
+++ src/components/products/productQuickview2.tsx
@@ -29,26 +29,26 @@ export default function ProductQuickview({
       Product Quickview2
     </button>
     <div className="modal fade" id="exampleModal2" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+      <div className="modal-dialog modal-dialog-centered modal-xl" role="document">
         <div className="modal-content">
           <div className="card card-product">
             <div className="card-body d-block d-lg-flex p-4">
               <button type="button" className="btn-close text-dark position-absolute end-0 me-4" data-bs-dismiss="modal" aria-label="Close"></button>
-              <div className="w-70 w-md-60 w-lg-30 text-center">
+              <div className="w-70 w-md-60 w-lg-50 text-center">
                 {(thumb_src) && 
                 <img className="w-100 rounded-3 mb-4" src={thumb_src} />
                 }
                 <a className="text-body" href="#">View details</a>
               </div>
-              <div className="w-100 w-lg-70 ps-4 mt-5 mt-lg-0 text-center">
+              <div className="w-100 w-lg-50 ps-4 mt-5 mt-lg-0 text-center">
                 <div className="d-flex justify-content-center">
                   {(title.length != 0) && 
-                    <h4 className="mt-5 mb-3 text-center">{title}</h4>
+                    <h3 className="mt-5 mb-3 text-center">{title}</h3>
                   }
                 </div>
                 {(price) && 
                   <>
-                    <h5 className="text-center">${price.toLocaleString()}</h5>
+                    <h4 className="text-center">${price.toLocaleString()}</h4>
                     <input className="opacity-0 d-none" defaultValue={price} />
                     <div className="d-flex justify-content-center mb-3">
                       <div className="d-flex align-items-center justify-content-center">
@@ -59,6 +59,8 @@ export default function ProductQuickview({
                     </div>
                   </>
                 }
+
+                <p className="text-center px-5">As we live, our hearts turn colder. Cause pain is what we go through as we become older.</p>
                 
                 {(colors.length != 0) && 
                   <>
@@ -69,7 +71,7 @@ export default function ProductQuickview({
                   </>
                 }
                 
-                <h6 className="mt-4">Size:</h6>
+                <h6 className="mt-4">Size</h6>
                 <div className="row mb-2">
                   <div className="col-6">
                     <div className="form-check">
@@ -91,10 +93,10 @@ export default function ProductQuickview({
                   </div>
                 </div>
 
-                <p>What should I buy? <span data-bs-toggle="tooltip" data-bs-placement="top" title="More information related to sizes" data-container="body" data-animation="true"><i className="fas fa-question-circle ms-1"></i></span></p>
+                <p className="mt-md-7 mb-4">What should I buy? <span data-bs-toggle="tooltip" data-bs-placement="top" title="More information related to sizes" data-container="body" data-animation="true"><i className="fas fa-question-circle ms-1"></i></span></p>
               
                 <div className="d-block text-center">
-                  <button className="btn btn-dark w-100">Add to Cart</button>
+                  <button className="btn btn-dark btn-lg w-100">Add to Cart</button>
                   <p className="mb-0 d-inline text-xl">
                     <i className="fas fa-shield-alt me-2"></i>
                     Premium Quality
