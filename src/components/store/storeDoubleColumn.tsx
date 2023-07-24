916c26d37d117dbe213ce8711d672a38a2df39fd
Stefan <stefan@creative-tim.com>
2023-07-24 18:57:22 +0300

    Update storeDoubleColumn.tsx

--- src/components/store/storeDoubleColumn.tsx
+++ src/components/store/storeDoubleColumn.tsx
@@ -3,129 +3,134 @@ export default function StoreNavigationDoubleColumn() {
     <>
     <div className="row mt-5">
       <div className="col-12 col-lg-6 mb-4 mb-lg-0">
-        <h6 className="w-100 pb-3 border-bottom">Clothing</h6>
+        <h6 className="w-100 pb-3 border-bottom">Store</h6>
         <div className="d-flex pt-2">
           <ul className="nav flex-column ms-n3">
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
                 Tops
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Dresses
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Designers
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Pants
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Tops
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Denim
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Collection
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Sweaters
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Texture
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                T-Shirts
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Sale
               </a>
             </li>
           </ul>
           <ul className="nav flex-column ms-6 ms-md-11 ms-lg-10">
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Jackets
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Prints
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Activewear
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Silk Edition
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Shorts
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Brands
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Swimwear
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Demos
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-body" href="#" target="_blank">
-                Browse All
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Showroom
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div className="col-6 col-lg-3">
-        <h6 className="w-100 pb-3 border-bottom">Accessories</h6>
-        <ul className="nav flex-column ms-n3 pt-2">
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Shoes
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Jewelry
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Handbags
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Socks
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Hats
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Browse All
-            </a>
-          </li>
-        </ul>
+      <div className="col-12 col-lg-6 mb-4 mb-lg-0">
+        <h6 className="w-100 pb-3 border-bottom">Categories</h6>
+        <div className="d-flex pt-2">
+          <ul className="nav flex-column ms-n3">
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Tops
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Designers
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Tops
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Collection
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Texture
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Sale
+              </a>
+            </li>
+          </ul>
+          <ul className="nav flex-column ms-6 ms-md-11 ms-lg-10">
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Prints
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Silk Edition
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Brands
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Demos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-body text-sm" href="#" target="_blank">
+                Showroom
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="col-6 col-lg-3">
-      <h6 className="w-100 pb-3 border-bottom ">Categories</h6>
-        <ul className="nav flex-column ms-n3 pt-2">
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              New Arrivals
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Sale
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Basic Tee
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-body" href="#" target="_blank">
-              Artwork Tees
-            </a>
-          </li>
-        </ul>
-      </div>
+      
     </div>
     </>
   );
