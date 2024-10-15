2344b8eea206325972d01d8cb5b7009bbe9e4b84
Luis Alfredo Rivera Acuña <1alfredorivera@gmail.com>
2024-10-14 22:21:20 -0700

    refactor: Add i18n support for UI navigation
    This commit adds support for internationalization (i18n) in the UI navigation component. The `ui.ts` file has been modified to include translations for the navigation items in English and Spanish. The `Layout.astro` and `Nav.astro` files have been updated to dynamically set the language attribute of the HTML element based on the current language. The `getLangFromUrl` and `useTranslations` functions have been added to the `utils.ts` file to handle language detection and translation respectively.
    
    Ref: #123

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  en: "English",
+  fr: "Français",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.about": "Acerca",
+  },
+} as const;
