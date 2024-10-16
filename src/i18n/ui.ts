727ef86b2adfc8581d7c03166450f17048a5597b
Luis Alfredo Rivera Acuña <1alfredorivera@gmail.com>
2024-10-15 18:38:33 -0700

    refactor: Update UI background color and remove unnecessary code
    This commit updates the background color of the UI in the Layout component and removes unnecessary code related to the Card component import in the index.astro file. The background color was commented out and the code was no longer needed, so it was removed. This improves the code cleanliness and ensures a consistent UI experience.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,16 +3,225 @@ export const languages = {
   fr: "Français",
 };
 
-export const defaultLang = "en";
+export const defaultLang = "es";
 
 export const ui = {
   en: {
     "nav.home": "Home",
     "nav.about": "About",
+    "nav.solutions": "solutions",
+    "nav.location": "location",
+    "nav.services": "services",
     "nav.twitter": "Twitter",
+    "header.title": "Sonora Precision",
+    "header.subtitle1":
+      "We turn your ideas into realities with the precision and quality you deserve.",
+    "header.subtitle2":
+      "Our team of experts works closely with you, ensuring every detail reflects your unique vision.",
+    "header.comp1.title": "Tailored solutions",
+    "header.comp1.subtitle":
+      "We adapt each project to your specific needs, delivering unique and satisfying results.",
+    "header.comp2.title": "Commitment to excellence",
+    "header.comp2.subtitle":
+      "Our focus on quality ensures that every phase of the project meets the highest standards.",
+    "header.comp3.title": "Ongoing communication",
+    "header.comp3.subtitle":
+      "We keep you informed throughout the entire process, ensuring you're aware of every update and adjustment.",
+    "mention.message":
+      "Precision is not just a goal, it's a journey. Every small adjustment brings you closer to perfection.",
+    "mention.author": "John T. Parsons",
+    "mention.position": "Pioneer in numerical control",
+    "about.title": "About the Company",
+    "about.body1": `We are a 100% Sonoran company, specializing in high-precision CNC machining, driven by a commitment to excellence and innovation in every project. Our focus lies in the manufacture of precision components, utilizing the latest in advanced technology and relying on a highly skilled team to ensure the highest standards of quality and accuracy in every piece we produce. With years of experience, we pride ourselves on delivering solutions tailored to the specific needs of our clients across various industries, from aerospace to automotive and beyond.`,
+    "about.body2": `At Sonora Precisión, we believe that our success is built on the satisfaction of our customers. This is why we consistently push the boundaries of what’s possible, ensuring that every project no matter how complex is, executed with precision, professionalism, and an unyielding attention to detail. Our goal is to not only meet industry standards but to exceed them, delivering parts and components that enhance the performance and reliability of your products.`,
+    "about.vision.title": "Our Vision",
+    "about.vision.body":
+      "To be the leading company in Mexico and the USA in the manufacture of precision parts and components, recognized for our technical excellence, relentless innovation, and unwavering commitment to quality and customer satisfaction. We aim to set the benchmark for precision manufacturing, forging long-lasting relationships with clients who trust us to provide them with unparalleled quality and service.",
+    "about.mision.title": "Our Mission",
+    "about.mision.body":
+      "At Sonora Precisión, we specialize in the production of high-precision parts, components, and mechanisms through the use of CNC technology, turning, and welding. We are dedicated to providing manufacturing solutions that consistently exceed our customers' expectations by:",
+    "about.footer":
+      "We take pride in being not just a manufacturer but a trusted partner, helping our clients achieve success through precision, innovation, and unmatched dedication to their goals.",
+    "about.mision.list": [
+      "Maintaining an unwavering commitment to quality at every stage of the production process.",
+      "Applying cutting-edge manufacturing techniques and state-of-the-art technology to ensure the highest level of precision and efficiency.",
+      "Continuously investing in the growth and development of our team, empowering them with the skills and knowledge to deliver exceptional results.",
+      "Fostering strong, collaborative partnerships with our clients to ensure their needs are fully understood and met with the highest level of satisfaction.",
+    ],
+    "solution.title": "Products and Solutions",
+    "solution.body":
+      "At Sonora Precision, we offer high-quality manufacturing solutions to meet your most demanding production needs. Our team of experts utilizes state-of-the-art technology to produce parts with the highest precision.",
+    "solution.cnc.title": "CNC Machining",
+    "solution.cnc.body":
+      "we offer are of the highest precision for a wide range of industries. We specialize in:",
+    "solution.cnc.list": [
+      "Machining complex parts with tight tolerances.",
+      "Using various materials such as aluminum, stainless steel, titanium, and engineering plastics.",
+      "Producing small to large batches with competitive lead times.",
+    ],
+    "solution.design.title": "Mechanical Design",
+    "solution.design.body":
+      "Our team of engineers and designers provides comprehensive mechanical design solutions, from conceptualization to the final manufacturing phase. We specialize in:",
+    "solution.design.list": [
+      "Developing 3D designs using SolidWorks and other advanced engineering software.",
+      "Optimizing designs to enhance manufacturability and reduce costs.",
+      "Performing finite element analysis (FEA) to ensure the functionality and strength of components before production.",
+      "Creating prototypes for testing and validation.",
+    ],
+    "solution.welding.title": "Welding",
+    "solution.welding.body":
+      "Our welding service is ideal for creating metal structures and joining high-precision parts. We offer:",
+    "solution.welding.list": [
+      "Technicians specialized in TIG, MIG, and arc welding.",
+      "Welding capabilities for various materials, including stainless steel, aluminum, and carbon steels.",
+      "Welding projects for both individual parts and complex assemblies.",
+      "Inspection and quality control to ensure the durability and strength of welds.",
+    ],
+    "solution.automation.title": "Automation",
+    "solution.automation.body":
+      "We specialize in the development of industrial automation projects. With a focus on improving efficiency and reducing operating costs, we offer:",
+    "solution.automation.list": [
+      "Design and development of customized automated systems.",
+      "Integration of industrial robots and automated control systems.",
+      "Implementation of remote monitoring and control systems.",
+      "Turnkey solutions to automate complete production lines.",
+    ],
+    "location.title": "You can find us here",
+    "location.button": "Visit us",
+    "services.title": "List of Services",
+    "services.list": [
+      "CNC Machining",
+      "Engineering Development and Mechanical",
+      "Design",
+      "Cutting and Welding",
+      "Automation",
+      "Industrial Equipment Manufacturing",
+      "Assembly of Electrical and Control Panels",
+      "Workstation Design",
+      "Manufacturing of Metal Structures",
+      "General Metalworking Projects",
+      "Heat Treatments",
+      "Grinding",
+    ],
+    "members.title": "Meet our leadership",
+    "members.subtitle":
+      "Discover the visionaries guiding our mission with expertise, integrity, and a commitment to excellence.",
+    "members.list": [
+      "Alejandro Guzmán",
+      "Alfredo Rivera",
+      "Daniel Rivera",
+      "Jesus Sigala",
+    ],
+    "footer.rights": "2024 Sonora Presicion, Inc. All rights reserved.",
   },
   es: {
     "nav.home": "Inicio",
-    "nav.about": "Acerca",
+    "nav.about": "Acerca de",
+    "nav.solutions": "Soluciones",
+    "nav.location": "Ubicación",
+    "nav.services": "Servicios",
+    "nav.twitter": "Twitter",
+    "header.title": "Sonora Precisión",
+    "header.subtitle1":
+      "Convertimos tus ideas en realidades con la precisión y calidad que mereces.",
+    "header.subtitle2":
+      "Nuestro equipo de expertos trabaja estrechamente contigo, asegurándose de que cada detalle refleje tu visión única.",
+    "header.comp1.title": "Soluciones personalizadas",
+    "header.comp1.subtitle":
+      "Adaptamos cada proyecto a tus necesidades específicas, entregando resultados únicos y satisfactorios.",
+    "header.comp2.title": "Compromiso con la excelencia",
+    "header.comp2.subtitle":
+      "Nuestro enfoque en la calidad asegura que cada fase del proyecto cumpla con los más altos estándares.",
+    "header.comp3.title": "Comunicación continua",
+    "header.comp3.subtitle":
+      "Te mantenemos informado durante todo el proceso, asegurando que estés al tanto de cada actualización y ajuste.",
+    "mention.message":
+      "La precisión no es solo una meta, es un viaje. Cada pequeño ajuste te acerca a la perfección.",
+    "mention.author": "John T. Parsons",
+    "mention.position": "Pionero en control numérico",
+    "about.title": "Acerca de la Empresa",
+    "about.body1":
+      "Somos una empresa 100% sonorense, especializada en el mecanizado CNC de alta precisión, impulsada por un compromiso con la excelencia y la innovación en cada proyecto. Nos enfocamos en la fabricación de componentes de precisión, utilizando lo último en tecnología avanzada y contando con un equipo altamente calificado para garantizar los más altos estándares de calidad y precisión en cada pieza que producimos. Con años de experiencia, nos enorgullece ofrecer soluciones adaptadas a las necesidades específicas de nuestros clientes en diversas industrias, desde aeroespacial hasta automotriz y más.",
+    "about.body2":
+      "En Sonora Precisión, creemos que nuestro éxito se construye sobre la satisfacción de nuestros clientes. Es por eso que constantemente desafiamos los límites de lo que es posible, asegurándonos de que cada proyecto, sin importar cuán complejo sea, se ejecute con precisión, profesionalismo y una atención inquebrantable al detalle. Nuestro objetivo no es solo cumplir con los estándares de la industria, sino superarlos, entregando partes y componentes que mejoren el rendimiento y la fiabilidad de tus productos.",
+    "about.vision.title": "Nuestra Visión",
+    "about.vision.body":
+      "Ser la empresa líder en México y Estados Unidos en la fabricación de partes y componentes de precisión, reconocida por nuestra excelencia técnica, innovación constante y compromiso inquebrantable con la calidad y la satisfacción del cliente. Nuestro objetivo es establecer el estándar para la fabricación de precisión, forjando relaciones duraderas con clientes que confían en nosotros para proporcionarles calidad y servicio inigualables.",
+    "about.mision.title": "Nuestra Misión",
+    "about.mision.body":
+      "En Sonora Precisión, nos especializamos en la producción de partes, componentes y mecanismos de alta precisión mediante el uso de tecnología CNC, torneado y soldadura. Estamos dedicados a proporcionar soluciones de manufactura que superen constantemente las expectativas de nuestros clientes mediante:",
+    "about.footer":
+      "Nos enorgullece ser no solo un fabricante, sino un socio de confianza, ayudando a nuestros clientes a alcanzar el éxito a través de precisión, innovación y una dedicación inigualable a sus metas.",
+    "about.mision.list": [
+      "Mantener un compromiso inquebrantable con la calidad en cada etapa del proceso de producción.",
+      "Aplicar técnicas de manufactura de vanguardia y tecnología de última generación para garantizar el más alto nivel de precisión y eficiencia.",
+      "Invertir continuamente en el crecimiento y desarrollo de nuestro equipo, capacitándolos con las habilidades y conocimientos para ofrecer resultados excepcionales.",
+      "Fomentar fuertes asociaciones colaborativas con nuestros clientes para asegurar que sus necesidades se comprendan y se satisfagan con el más alto nivel de satisfacción.",
+    ],
+    "solution.title": "Productos y Soluciones",
+    "solution.body":
+      "En Sonora Precisión, ofrecemos soluciones de manufactura de alta calidad para satisfacer tus necesidades de producción más exigentes. Nuestro equipo de expertos utiliza tecnología de punta para producir piezas con la más alta precisión.",
+    "solution.cnc.title": "Mecanizado CNC",
+    "solution.cnc.body":
+      "Ofrecemos mecanizado de alta precisión para una amplia gama de industrias. Nos especializamos en:",
+    "solution.cnc.list": [
+      "Mecanizado de piezas complejas con tolerancias ajustadas.",
+      "Uso de diversos materiales como aluminio, acero inoxidable, titanio y plásticos de ingeniería.",
+      "Producción de lotes pequeños a grandes con tiempos de entrega competitivos.",
+    ],
+    "solution.design.title": "Diseño Mecánico",
+    "solution.design.body":
+      "Nuestro equipo de ingenieros y diseñadores ofrece soluciones completas de diseño mecánico, desde la conceptualización hasta la fase final de manufactura. Nos especializamos en:",
+    "solution.design.list": [
+      "Desarrollar diseños 3D utilizando SolidWorks y otros software de ingeniería avanzada.",
+      "Optimizar diseños para mejorar la manufacturabilidad y reducir costos.",
+      "Realizar análisis de elementos finitos (FEA) para garantizar la funcionalidad y resistencia de los componentes antes de la producción.",
+      "Crear prototipos para pruebas y validación.",
+    ],
+    "solution.welding.title": "Soldadura",
+    "solution.welding.body":
+      "Nuestro servicio de soldadura es ideal para crear estructuras metálicas y unir piezas de alta precisión. Ofrecemos:",
+    "solution.welding.list": [
+      "Técnicos especializados en soldadura TIG, MIG y de arco.",
+      "Capacidades de soldadura para diversos materiales, incluyendo acero inoxidable, aluminio y aceros al carbono.",
+      "Proyectos de soldadura para piezas individuales y ensamblajes complejos.",
+      "Inspección y control de calidad para garantizar la durabilidad y resistencia de las soldaduras.",
+    ],
+    "solution.automation.title": "Automatización",
+    "solution.automation.body":
+      "Nos especializamos en el desarrollo de proyectos de automatización industrial. Con un enfoque en mejorar la eficiencia y reducir costos operativos, ofrecemos:",
+    "solution.automation.list": [
+      "Diseño y desarrollo de sistemas automatizados personalizados.",
+      "Integración de robots industriales y sistemas de control automatizados.",
+      "Implementación de sistemas de monitoreo y control remoto.",
+      "Soluciones llave en mano para automatizar líneas de producción completas.",
+    ],
+    "location.title": "Encuéntranos aquí",
+    "location.button": "Visítanos",
+    "services.title": "Lista de Servicios",
+    "services.list": [
+      "Mecanizado CNC",
+      "Desarrollo de Ingeniería y Diseño Mecánico",
+      "Corte y Soldadura",
+      "Automatización",
+      "Fabricación de Equipos Industriales",
+      "Ensamblaje de Tableros Eléctricos y de Control",
+      "Diseño de Estaciones de Trabajo",
+      "Fabricación de Estructuras Metálicas",
+      "Proyectos de Metalistería General",
+      "Tratamientos Térmicos",
+      "Rectificado",
+    ],
+    "members.title": "Conoce a nuestro liderazgo",
+    "members.subtitle":
+      "Descubre a los visionarios que guían nuestra misión con experiencia, integridad y un compromiso con la excelencia.",
+    "members.list": [
+      "Alejandro Guzmán",
+      "Alfredo Rivera",
+      "Daniel Rivera",
+      "Jesús Sigala",
+    ],
+    "footer.rights":
+      "2024 Sonora Precisión, Inc. Todos los derechos reservados.",
   },
 } as const;
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -8,6 +8,7 @@ export function getLangFromUrl(url: URL) {
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return (ui[lang] as Record<string, string>)[key] || ui[defaultLang][key];
+    // @ts-ignore
+    return ui[lang][key] || ui[defaultLang][key];
   };
 }
